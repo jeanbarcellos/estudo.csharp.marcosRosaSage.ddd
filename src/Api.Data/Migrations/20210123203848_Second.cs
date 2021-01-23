3616d3ed1d5aa3483575a14b6fdde97732c815ff
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-23 17:46:12 -0300

    Adição da segunda migração

---
+++ src/Api.Data/Migrations/20210123203848_Second.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "user",
+                newName: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "user",
+                newName: "Id");
+        }
+    }
+}
